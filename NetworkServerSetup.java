47a2bfe2c92375b9ff06b7ad73ec1180b26777ad
vjkim64 <vjain40@gatech.edu>
2014-11-26 18:17:38 -0500

    yay

--- NetworkServerSetup.java
+++ NetworkServerSetup.java
@@ -21,7 +21,6 @@ import java.util.Timer;
 import java.util.TimerTask;
 import java.util.concurrent.ConcurrentHashMap;
 
-
 public class NetworkServerSetup {
 	private static int current;
 	private static int max;
@@ -48,26 +47,6 @@ public class NetworkServerSetup {
 		}
 		@Override
 		public void run() {
-			// TODO Auto-generated method stub
-			//System.out.println("#noob");
-			//timer.cancel();
-			//System.out.println("yolo");
-			//timer.
-			/*timer = new Timer();
-			timer.schedule(this, 1000);*/
-			// add node to end of queue
-			// if being transferred right now == 0 resend
-			/*if(ncs.containsSeqNum(node.getSeqNum())){
-				if(map.size()==0){
-					try{
-						sendMore();
-					} catch (IOException e){
-
-					}
-				}
-				return;
-			}*/
-
 			timer.cancel();
 			map.remove(node.getSeqNum());
 			ncs.removeCurrent(node.getSeqNum());
@@ -76,13 +55,6 @@ public class NetworkServerSetup {
 			ncs.addPacket(node.getSeqNum());
 			//ncs.addToEnd(node);
 			if(map.size()==0){
-				max = 1;
-				/*try {
-					//restart();
-				} catch (IOException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				}*/
 				try{
 					sendMore();
 				} catch (IOException e){
@@ -91,21 +63,6 @@ public class NetworkServerSetup {
 			}
 		}
 		public void sendMore() throws IOException{
-			//System.out.println(node.getSeqNum());
-			//System.out.println(ncs.ftpComplete());
-			/*int s = 8000;
-			Node node = ncs.sendNextPacket();
-			System.out.println("prob is here");
-			if(node==null) return;
-			if(ncs.containsCurrent(node.getSeqNum())) return;
-			InetAddress IPAddress = InetAddress.getByName("localhost");
-			byte[] toSend = node.getPacket();
-			DatagramPacket sendPacket = new DatagramPacket(toSend, toSend.length, IPAddress, s);
-			Timer time = new Timer();
-			TimerTaskMe tsk = new TimerTaskMe(time, node);
-			time.schedule(tsk, 2000);
-			map.put(node.getSeqNum(), time);
-			clientSocket.send(sendPacket);*/
 			sendNextPacket();
 		}
 	}
@@ -163,41 +120,6 @@ public class NetworkServerSetup {
 		Thread thread = new Thread(new CheckClientReception(ncs, clientSocket));
 		thread.start();
 		sendNextPacket();
-		//clientSocket.close();
-		//restart();
-		/*Node node = ncs.sendNextPacket();
-		do{
-			if(node==null) continue;
-			System.out.println(node.getSeqNum());
-			toSend = node.getPacket();
-			sendPacket = new DatagramPacket(toSend, toSend.length, IPAddress, s);
-			Timer time = new Timer();
-			TimerTaskMe tsk = new TimerTaskMe(time, node);
-			time.schedule(tsk, 2000);
-			map.put(node.getSeqNum(), time);
-			clientSocket.send(sendPacket);
-			receiveData = new byte[1024];
-			here:{
-				receivePacket = new DatagramPacket(receiveData, receiveData.length);
-				clientSocket.receive(receivePacket);
-				ByteBuffer gotten = ByteBuffer.wrap(receivePacket.getData());
-				if(!checkData(receiveData, gotten.getShort(0))){
-					break here;
-				}
-				if (receivePacket.getData()[12]==1&&map.containsKey(gotten.getInt(6))){
-					Timer tmp = map.remove(gotten.getInt(6)); // can be error if not in map...
-					tmp.cancel();
-					ncs.recievedAck(gotten.getInt(6));
-					//map.get(gotten.getInt(6)).cancel(); /
-					current++;
-					if(current==max){
-						max++;
-						current = 0;
-					}
-					node = ncs.sendNextPacket();
-				}
-			}
-		}while(!ncs.ftpComplete());*/
 	}
 	public static boolean checkData(byte[] data, short sum){
 		short checkSum = 0;
@@ -227,6 +149,7 @@ public class NetworkServerSetup {
 			if(current==max){
 				max++;
 				current = 0;
+				sendNextPacket(); // conjestion control
 			}
 			sendNextPacket();
 		}
